e2a8d4acffa16f80e5fa010865240dff4215f127
Holokai <cveith@gmail.com>
2015-07-21 15:23:58 -1000

    Added support for an array of filters

--- root/_model.js
+++ root/_model.js
@@ -3,9 +3,20 @@ define(function(){
 	var Backbone = require('backbone');
 
 	return Backbone.Model.extend({
-		urlRoot : '{%= BaseUrl %}{%= ModelUrl %}',
+		urlRoot : '{%= BaseUrl %}{%= ModelUrl %}/',
 		url:function(){
-			return this.urlRoot;
+			var filter = this.get('filter');
+			var filterUrl = '';
+			//if a filter has been set, iterate through it and find the associated property/value pairs
+			if(! _.isEmpty(filter)){
+				filterUrl = '?Filter=['
+				for(var i=0;i<filter.length;i++){
+					filterUrl+='{property:'+filter[i].property+',value:'+filter[i].value+'},';
+				}
+				filterUrl = filterUrl.substring(0,filterUrl.length-1)+']';
+			}
+			return this.urlRoot+filterUrl;
+		
 		},
 		//attrs contains key/value of model data
 		//key: model attribute name
