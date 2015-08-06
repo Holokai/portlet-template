f990bfb32d395c236244a4dfa4d1a3e3f7f79324
Chris Veith <chris_veith@datahouse.com>
2015-08-06 09:57:27 -1000

    Added collection

---
+++ root/_collection.js
@@ -0,0 +1,30 @@
+
+define(function(){
+	const Backbone = require('backbone');
+	const IPS = require('../base/IPSModelCollection')
+    const builder = require('civicsUtils/buildUrlParameters');
+
+	return IPS.extend({
+		urlRoot : '{%= BaseUrl %}{%= ModelUrl %}/',
+		url:function(){
+			var parameters = ''
+			if(this.urlParms!=undefined){
+				builder.buildUrlParameters(this.urlParms)
+			}
+			return this.urlRoot+parameters;
+		},
+		//attrs contains key/value of model data
+		//key: model attribute name
+		//value: model attribute value
+		validate:function(attrs,options){
+			//errors object to hold error messages.
+			//key: portion of jQuery selector for invalid field
+			//value:Error Message
+			var errors = {};
+			//do Validations
+			if(! _.isEmpty(errors)){
+				return errors;
+			}
+		}
+	});
+});
